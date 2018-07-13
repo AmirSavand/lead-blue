554272f58cbe033e10cb9a082c7fe6b3533a725f
Amir Savand <amir@savandbros.com>
2018-07-13 14:42:48 +0400

    Add score and time text

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     public int score = 0;
     public float time = 60;
 
+    public Text textScore;
+    public Text textTime;
+
     public FloorSpawner floorSpawner;
 
     private void Update()
     {
         // Decrease time
-        time -= Time.deltaTime;
+        time = Mathf.Clamp(time + Time.deltaTime, 0, Time.deltaTime);
+
+        // Update time text (UI)
+        textTime.text = Mathf.Round(time).ToString();
     }
 
     public void OnHit(Hit hit)
@@ -19,7 +26,10 @@ public class Game : MonoBehaviour
         floorSpawner.Spawn();
 
         // Handle score and time
-        score += hit.score;
-        time += hit.time;
+        score = Mathf.Clamp(score + hit.score, 0, score + hit.score);
+        time = Mathf.Clamp(time + hit.time, 0, time + hit.time);
+
+        // Update score text (UI)
+        textScore.text = score.ToString();
     }
 }
