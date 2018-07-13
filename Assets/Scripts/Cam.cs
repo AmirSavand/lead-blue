23a1528a4508a39fa624387cd25fc6a27e0167f3
Amir Savand <amir@savandbros.com>
2018-07-13 17:30:03 +0400

    Add game over UI

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -6,21 +6,27 @@ public class Cam : MonoBehaviour
 
     public float smoothTime = 0.1f;
     public float distance = 10f;
+    public bool faceUp;
 
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
-        // No target, no update
-        if (!target)
+        // No target, no follow
+        if (target)
         {
-            return;
+            // Position to smoothly move to (only on Z axis)
+            Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, target.position.z - distance);
+
+            // Smoothly move the camera
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
 
-        // Position to smoothly move to (only on Z axis)
-        Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, target.position.z - distance);
-
-        // Smoothly move the camera
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        // Face up
+        if (faceUp)
+        {
+            // Face up smoothly
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-30, 0, 0), Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,23 +1,63 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    Menu,
+    Run,
+    Pause,
+    Over
+}
 
 public class Game : MonoBehaviour
 {
+    public GameState gameState = GameState.Menu;
+
     public int score = 0;
     public float time = 60;
 
     public Text textScore;
     public Text textTime;
 
+    public GameObject uiMenu;
+    public GameObject uiRun;
+    public GameObject uiPause;
+    public GameObject uiOver;
+
+    public Cam cam;
+    public Player player;
     public FloorSpawner floorSpawner;
 
-    private void Update()
+    private void Start()
+    {
+        // Setup state UIs
+        UpdateStateUis();
+    }
+
+    void Update()
     {
         // Decrease time
-        time = Mathf.Clamp(time + Time.deltaTime, 0, Time.deltaTime);
+        time = Mathf.Clamp(time - Time.deltaTime, 0, time);
 
         // Update time text (UI)
         textTime.text = Mathf.Round(time).ToString();
+
+        // Ran out of time (when running)
+        if (time == 0 && gameState == GameState.Run)
+        {
+            // Game over
+            gameState = GameState.Over;
+
+            // Face camera up
+            cam.faceUp = true;
+
+            // Destroy player
+            Destroy(player.gameObject);
+
+            // Update UI as well
+            UpdateStateUis();
+        }
     }
 
     public void OnHit(Hit hit)
@@ -31,5 +71,24 @@ public class Game : MonoBehaviour
 
         // Update score text (UI)
         textScore.text = score.ToString();
+    }
+
+    /**
+     * Update state of all UIs based on game state
+     */
+    public void UpdateStateUis()
+    {
+        uiMenu.SetActive(gameState == GameState.Menu);
+        uiRun.SetActive(gameState == GameState.Run);
+        uiPause.SetActive(gameState == GameState.Pause);
+        uiOver.SetActive(gameState == GameState.Over);
+    }
+
+    /**
+     * Restart game (scene)
+     */
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
