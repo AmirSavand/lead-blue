deb12e1e2d552dc71d67c6dfb4bff4255ce4af51
Amir Savand <amir@savandbros.com>
2018-07-19 19:24:20 +0400

    Destroy out of edge only when game is running

--- Assets/Scripts/Edge.cs
+++ Assets/Scripts/Edge.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Edge : MonoBehaviour
 {
+    private Game game;
+
+    void Start()
+    {
+        // Get inits
+        game = Game.Get();
+    }
+
     void OnTriggerExit(Collider other)
     {
-        if (other.GetComponentInParent<Rigidbody>())
+        // Destroy if game is running
+        if (game.gameState == GameState.Run)
         {
-            Destroy(other.GetComponentInParent<Rigidbody>().gameObject);
-        }
-        else
-        {
-            Destroy(other.gameObject);
+            // If other has a parent rb
+            if (other.GetComponentInParent<Rigidbody>())
+            {
+                // Destroy the parent
+                Destroy(other.GetComponentInParent<Rigidbody>().gameObject);
+            }
+            // Destroy self
+            else
+            {
+                Destroy(other.gameObject);
+            }
         }
     }
 }
