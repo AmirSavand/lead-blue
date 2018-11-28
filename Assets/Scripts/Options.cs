618c280a70ba15eb8d8faa1a1724765587e1c1a0
Amir Savand <amir@savandbros.com>
2018-11-28 22:10:17 +0400

    Add option to mute game and music sound

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class Options : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public GameObject soundOff;
+    public GameObject musicOff;
+
+    void Start()
+    {
+        UpdateOptions(false);
+    }
+
+    void UpdateOptions(bool commit = true)
+    {
+        soundOff.SetActive(!Storage.EnableSound);
+        musicOff.SetActive(!Storage.EnableMusic);
+
+        audioMixer.SetFloat("volGame", Storage.EnableSound ? 0 : -80);
+        audioMixer.SetFloat("volMusic", Storage.EnableMusic ? 0 : -80);
+
+        if (commit)
+        {
+            Storage.Save();
+        }
+    }
+
+    public void ToggleSound()
+    {
+        Storage.EnableSound = !Storage.EnableSound;
+        UpdateOptions();
+    }
+
+    public void ToggleMusic()
+    {
+        Storage.EnableMusic = !Storage.EnableMusic;
+        UpdateOptions();
+    }
+}
---
+++ Assets/Scripts/Storage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    public static int HighScore;
+
+    public static bool EnableSound;
+    public static bool EnableMusic;
+
+    void Awake()
+    {
+        // Keep it between scenes
+        DontDestroyOnLoad(gameObject);
+
+        // Destroy it if it's a duplicate
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Load stats
+        HighScore = PlayerPrefs.GetInt("stat-high-score");
+
+        // Load switches (bools)
+        EnableSound = PlayerPrefs.GetInt("enable-sound", 1) == 1;
+        EnableMusic = PlayerPrefs.GetInt("enable-music", 1) == 1;
+    }
+
+    /**
+     * Commit all data to storage.
+     */
+    public static void Save()
+    {
+        // Set stats
+        PlayerPrefs.SetInt("stat-high-score", HighScore);
+
+        // Set switches
+        PlayerPrefs.SetInt("enable-sound", EnableSound ? 1 : 0);
+        PlayerPrefs.SetInt("enable-music", EnableMusic ? 1 : 0);
+
+        // Commit
+        PlayerPrefs.Save();
+    }
+}
